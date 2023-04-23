48886607814738447178db6f119e33c4a4870291
majorchork <majorchork@gmail.com>
2023-04-23 08:19:05 +0100

    feat: add cors permissions

--- cmd/server/router.go
+++ cmd/server/router.go
@@ -1,16 +1,26 @@
 package server
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/majorchork/tech-crib-africa/internal/controller"
 	"github.com/majorchork/tech-crib-africa/internal/middleware"
 	"log"
+	"time"
 )
 
 func DefineRoutes(handler *controller.Handler) *gin.Engine {
 	log.Println("Routes defined")
 
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"POST", "GET", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r := router.Group("/api/v1")
 	{
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/majorchork/tech-crib-africa
 go 1.18
 
 require (
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.0
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang/mock v1.6.0
